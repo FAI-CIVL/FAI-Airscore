9464dc149c321ad76a23dfac7d2c2af18556df4a
Geoff Wong <geoff.wong@gmail.com>
2016-12-30 23:36:10 +1100

    Sigh .. retrieve older version.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require 'authorisation.php';
 require 'hc.php';
 require 'format.php';
 
-    function tracktable($link, $order, $start, $pilot)
+    function tracktable($link, $restrict, $order, $start, $pilot)
     {
         //<div id=\"comments\"><ol>";
 
@@ -11,12 +11,12 @@ require 'format.php';
         $long = [];
         if ($pilot > 0)
         {
-            $sql = "select T.*, P.*, CTT.*, L.* from tblTrack T, tblPilot P, tblComTaskTrack CTT, tblWaypoint W, tblLaunchSite L where W.wptPosition=0 and (abs(W.wptLatDecimal-L.lauLatDecimal)+abs(W.wptLongDecimal-L.lauLongDecimal)) < 1.0 and T.traPk=W.traPk and CTT.traPk=T.traPk and T.pilPk=P.pilPk $order";
-            $sql = "select T.*, P.*, CTT.*, L.* from tblTrack T, tblPilot P, tblComTaskTrack CTT, tblWaypoint W left outer join tblLaunchSite L on (abs(W.wptLatDecimal-L.lauLatDecimal)+abs(W.wptLongDecimal-L.lauLongDecimal)) < 1.0 where W.wptPosition=0 and T.traPk=W.traPk and CTT.traPk=T.traPk and T.pilPk=P.pilPk $order";
+            //$sql = "select T.*, P.*, CTT.*, L.* from tblTrack T, tblPilot P, tblComTaskTrack CTT, tblWaypoint W, tblLaunchSite L where W.wptPosition=0 and (abs(W.wptLatDecimal-L.lauLatDecimal)+abs(W.wptLongDecimal-L.lauLongDecimal)) < 1.0 and T.traPk=W.traPk and CTT.traPk=T.traPk and T.pilPk=P.pilPk $order";
+            $sql = "select T.*, P.*, CTT.*, L.* from tblTrack T, tblPilot P, tblComTaskTrack CTT, tblWaypoint W left outer join tblLaunchSite L on (abs(W.wptLatDecimal-L.lauLatDecimal)+abs(W.wptLongDecimal-L.lauLongDecimal)) < 0.2 where W.wptPosition=0 and T.traPk=W.traPk and CTT.traPk=T.traPk and T.pilPk=P.pilPk $restrict group by T.traPk $order";
         }
         else
         {
-            $sql = "select T.*, P.*, CTT.* from tblTrack T, tblPilot P, tblComTaskTrack CTT where CTT.traPk=T.traPk and T.traLength > 0 and T.pilPk=P.pilPk $order";
+            $sql = "select T.*, P.*, CTT.* from tblTrack T, tblPilot P, tblComTaskTrack CTT where CTT.traPk=T.traPk and T.traLength > 0 and T.pilPk=P.pilPk $restrict group by T.traPk $order";
         }
         $result = mysql_query($sql,$link) or die("Invalid track table " . mysql_error());
         $num = mysql_num_rows($result);
@@ -29,12 +29,12 @@ require 'format.php';
             if ($pilot > 0)
             {
                 $name = $row['lauLaunch'] . ', ' . $row['lauRegion'];
-                $long[] = array ("$count.", "<a href=\"tracklog_map.php?trackid=$id&comPk=$comPk\">$dist kms</a>", substr($row['traStart'],0,10), "<a href=\"index.php?pil=$pilPk\">$name<a>");
+                $long[] = array ("$count.", "<a href=\"tracklog_map.php?trackid=$id&comPk=$comPk\">$dist kms</a>", substr($row['traStart'],0,10), "<a href=\"pilot.php?pil=$pilPk\">$name<a>");
             } 
             else
             {
                 $name = $row['pilFirstName'] . ' ' . $row['pilLastName'];
-                $long[] = array ("$count.", "<a href=\"tracklog_map.php?trackid=$id&comPk=$comPk\">$dist kms</a>", substr($row['traStart'],0,10), "<a href=\"index.php?pil=$pilPk\">$name<a>");
+                $long[] = array ("$count.", "<a href=\"tracklog_map.php?trackid=$id&comPk=$comPk\">$dist kms</a>", substr($row['traStart'],0,10), "<a href=\"pilot.php?pil=$pilPk\">$name<a>");
             }
             $count++;
         }
@@ -88,7 +88,7 @@ require 'format.php';
     echo "<div id=\"content\">";
     echo "<div id=\"text\">";
 
-    if ($order == 'long' || $order == 'recent')
+    if ($order == 'long' || $order == 'recent' || $order == 'best')
     {
         $lhead = "Recent";
         $clause = "by T.traStart desc";
@@ -97,10 +97,16 @@ require 'format.php';
             $lhead = "Longest";
             $clause = "by T.traLength desc";
         }
+        elseif ($order == 'best')
+        {
+            $lhead = "Best";
+            $clause = "by T.traScore desc";
+        }
         $num = 25;
-        $sort = "${comstr}${pilstr}order $clause limit $start,$num";
+        $restrict = "${comstr}${pilstr}";
+        $sort = "order $clause limit $start,$num";
         echo "<h1><span>$lhead " . ($start+1) . "-" . ($start+$num) . "</span></h1>\n";
-        $numout = tracktable($link, $sort, $start, $pilot); 
+        $numout = tracktable($link, $restrict, $sort, $start, $pilot); 
         if ($start > 1)
         {
             echo "<b class=\"left\"><a href=\"index.php?${comclause}${piln}order=$order&start=" . ($start-$num) . "\">Prev 25</a></b>\n";
@@ -113,16 +119,18 @@ require 'format.php';
     else
     {
         $num = 10;
-        $sort = "${comstr}${pilstr}order by T.traLength desc limit $num";
+        $restrict = "${comstr}${pilstr}";
+        $sort = "order by T.traLength desc limit $num";
         echo "<h1><span>Longest 10</span></h1>\n";
-        if (tracktable($link, $sort, $start, $pilot) == $num)
+        if (tracktable($link, $restrict, $sort, $start, $pilot) == $num)
         {
             echo "<b class=\"right\"><a href=\"index.php?${comclause}${piln}order=long&start=" . ($start+$num) . "\">Next 25</a></b>\n";
         }
 
-        $sort = "${comstr}${pilstr}order by T.traStart desc limit $num";
+        $restrict = "${comstr}${pilstr}";
+        $sort = "order by T.traStart desc limit $num";
         echo "<h1><span>Recent 10</span></h1>\n";
-        if (tracktable($link, $sort, $start, $pilot) == $num)
+        if (tracktable($link, $restrict, $sort, $start, $pilot) == $num)
         {
             echo "<b class=\"right\"><a href=\"index.php?${comclause}${piln}order=recent&start=" . ($start+$num) . "\">Next 25</a></b>\n";
         }
@@ -132,7 +140,14 @@ require 'format.php';
     //echo "<div id=\"image\"><img src=\"images/pilots.jpg\" alt=\"Pilots Flying\"/></div>";
     echo "<div id=\"sideBar\">";
     hcopencomps($link);
-    hcclosedcomps($link);
+    if ($comp > 1)
+    {
+        hcclosedcomps($link, $title);
+    }
+    else
+    {
+        hcclosedcomps($link);
+    }
     hcregion($link);
     echo "</div>";
     hcimage($link,$comp);
