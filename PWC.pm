cdd1653f9dcb756cba3d5c1666de05bb65c3a512
kuaka <stuart.mackintosh@gmail.com>
2017-12-01 22:59:22 +0100

    added modified gap points_weight (changed goal ratio to ess ratio
    added modified gap pilot_departure_leadout to use mincoeff2
    
    added modified gap pilot_speed for pspeed.
    
    all changes to align to pwca 2016

--- PWC.pm
+++ PWC.pm
@@ -111,17 +111,213 @@ sub day_quality
     return ($distance,$time,$launch,$stopv);
 }
 
+sub points_weight
+{
+    my ($self, $task, $taskt, $formula) = @_;
+    my $quality;
+    my $distweight;
+    my ($Adistance, $Aspeed, $Astart, $Aarrival);
+    my $x;
+    my $dem;
+    my $speedweight;
+
+
+    $quality = $taskt->{'quality'};
+    $x = $taskt->{'ess'} / $taskt->{'launched'}; #changed from goal to ess as per pwca
+
+    if ($formula->{'weightdist'} eq 'post2014')
+    {
+        $distweight = 0.9-1.665*$x+1.713*$x*$x-0.587*$x*$x*$x;
+    }
+    else
+    {
+        $distweight = 1-0.8*sqrt($x);
+    }
+
+    # 1998 - 1999 - (speed 6 / start 1 / arrival 1) / 8
+    # 2000 - 2007 - (5.6 / 1.4 / 1) / 8
+    $Adistance = 1000 * $quality * $distweight;
+    $Astart = 1000 * $quality * (1-$distweight) * $formula->{'weightstart'};
+    $Aarrival = 1000 * $quality * (1-$distweight) * $formula->{'weightarrival'};
+    $speedweight = $formula->{'weightspeed'};
+
+    if ($task->{'arrival'} eq 'off')
+    {
+        $Aarrival = 0;
+        $speedweight += $formula->{'weightarrival'};
+    }
+
+    if ($task->{'departure'} eq 'off')
+    {
+        $Astart = 0;
+        $speedweight += $formula->{'weightstart'};
+    }
+    $Aspeed = 1000 * $quality * (1-$distweight) * $speedweight;
+
+    if ($formula->{'scaletovalidity'})
+    {
+        $dem = $Adistance + $Aspeed + $Aarrival + $Astart;
+        $Adistance = 1000 * $quality * $Adistance / $dem;
+        $Aspeed = 1000 * $quality * $Aspeed / $dem;
+        $Aarrival = 1000 * $quality * $Aarrival / $dem;
+        $Astart =  1000 * $quality * $Astart / $dem;
+    }
+
+    print "points_weight: (", $formula->{'version'}, ") Adist=$Adistance, Aspeed=$Aspeed, Astart=$Astart, Aarrival=$Aarrival\n";
+    return ($Adistance, $Aspeed, $Astart, $Aarrival);
+
+}
+
+sub pilot_departure_leadout
+{
+    my ($self, $formula, $task, $taskt, $pil, $Astart, $Aspeed) = @_;
+    my $x = 0;
+    my $Parrival = 0;
+    my $Cmin = $taskt->{'mincoeff2'};
+
+    # Pilot departure score
+    # print Dumper($pil);
+
+    my $Pdepart = 0;
+    if ($task->{'departure'} eq 'leadout')
+    {
+        print "    leadout: ", $pil->{'coeff'}, ", $Cmin\n";
+        if ($pil->{'coeff'} > 0)
+        {
+            if ($pil->{'coeff'} <= $Cmin)
+            {
+                $Pdepart = $Astart;
+            }
+            elsif ($Cmin <= 0)
+            {
+                # this shouldn't happen
+                $Pdepart = 0;
+            }
+            else
+            {
+                $Pdepart = $Astart * (1-(($pil->{'coeff'}-$Cmin)*($pil->{'coeff'}-$Cmin)/sqrt($Cmin))**(1/3));
+            }
+        }
+    }
+    elsif ($task->{'departure'} eq 'kmbonus')
+    {
+        my $kmarr = $taskt->{'kmmarker'};
+        my @tmarker = @$kmarr;
+
+        #print "TMARKER=", Dumper(\@tmarker);
+        # KmBonus award points
+        if (scalar(@tmarker) > 0)
+        {
+            #print "Astart=$Astart PKM=", Dumper($pil->{'kmmarker'});
+            for my $km (1..scalar(@tmarker))
+            {
+                if ($pil->{'kmmarker'}->[$km] > 0 && $tmarker[$km] > 0)
+                {
+                    #$x = 1 - ($pil->{'kmmarker'}->[$km] - $tmarker[$km]) / ($tmarker[$km]/4);
+                    $x = 1 - ($pil->{'kmmarker'}->[$km] - $tmarker[$km]) / 600;
+                    if ($x > 0)
+                    {
+                        $Pdepart = $Pdepart + (0.2+0.037*$x+0.13*($x*$x)+0.633*($x*$x*$x));
+                    }
+                }
+            }
+            $Pdepart = $Pdepart * $Astart * 1.25 / floor($task->{'ssdistance'}/1000.0);
+            if ($Pdepart > $Astart)
+            {
+                $Pdepart = $Astart;
+            }
+        }
+        else
+        {
+            $Pdepart = 0;
+        }
+    }
+    elsif ($task->{'departure'} eq 'off')
+    {
+        $Pdepart = 0;
+        # print "    depart off\n";
+    }
+    else
+    {
+        # Normal departure points ..
+        $x = ($pil->{'startSS'} - $taskt->{'firstdepart'})/$formula->{'nomtime'};
+        # print "    normal departure x=$x\n";
+        if ($x < 1/2 && $pil->{'time'} > 0)
+        {
+            my $Pspeed = $self->pilot_speed($formula, $task, $taskt, $pil, $Aspeed);
+
+            $Pdepart = $Pspeed*$Astart/$Aspeed*(1-6.312*$x+10.932*($x*$x)-2.990*($x*$x*$x));
+            #$Pdepart = $Astart*(1-6.312*$x+10.932*($x*$x)-2.990*($x*$x*$x));
+            #$Pdepart = $Aspeed/6*(1-6.312*$x+10.932*($x*$x)-2.990*($x*$x*$x));
+        }
+    }
+
+    # Sanity
+    if (0+$Pdepart != $Pdepart)
+    {
+        # print "    Pdepart is nan\n";
+        $Pdepart = 0;
+    }
+
+    if ($Pdepart < 0)
+    {
+        $Pdepart = 0;
+    }
+
+    # print "    Pdepart: $Pdepart\n";
+    return $Pdepart;
+}
+
+sub pilot_speed
+{
+    my ($self, $formula, $task, $taskt, $pil, $Aspeed) = @_;
+
+    my $Tmin = $taskt->{'fastest'};
+    my $Pspeed;
+    my $Ptime = 0;
+
+    if ($pil->{'time'} > 0)
+    {
+        $Ptime = $pil->{'time'};
+    }
+
+    print $pil->{'traPk'}, " Ptime: $Ptime, Tmin=$Tmin\n";
+
+    if ($Ptime > 0)
+    {
+        $Pspeed = $Aspeed * (1-(($Ptime-$Tmin)/3600/sqrt($Tmin/3600))**(5/6));  #Changed from 2/3 to 5/6 as per PWCA
+    }
+    else
+    {
+        $Pspeed = 0;
+    }
+
+    if ($Pspeed < 0)
+    {
+        $Pspeed = 0;
+    }
+
+    if (0+$Pspeed != $Pspeed)
+    {
+        print $pil->{'traPk'} , " Pspeed is nan: pil->{'time'}=", $Ptime, "\n";
+        $Pspeed = 0;
+    }
+
+    return $Pspeed;
+}
+
+
 sub ordered_results
 {
     my ($self, $dbh, $task, $taskt, $formula) = @_;
     my @pilots;
 
-    # Get all pilots and process each of them 
+    # Get all pilots and process each of them
     # pity it can't be done as a single update ...
     $dbh->do('set @x=0;');
     $sth = $dbh->prepare("select \@x:=\@x+1 as Place, tarPk, tarDistance, tarSS, tarES, tarPenalty, tarResultType, tarLeadingCoeff2, tarGoal, tarLastAltitude from tblTaskResult where tasPk=? and tarResultType <> 'abs' order by case when (tarGoal=0 or tarES is null) then -999999 else tarLastAltitude end, tarDistance desc");
     $sth->execute($task->{'tasPk'});
-    while ($ref = $sth->fetchrow_hashref()) 
+    while ($ref = $sth->fetchrow_hashref())
     {
         my %taskres;
 
