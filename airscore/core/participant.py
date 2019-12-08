725cddcf02d81a52ffc7463fe350820ce5549cab
Antonio Golfari <antoniogolfari@gmail.com>
2019-12-09 00:10:15 +0100

    Create a function to import a Participant list from Airtribune Excel file

--- airscore/core/participant.py
+++ airscore/core/participant.py
@@ -20,13 +20,13 @@ class Participant(object):
     """Partecipant definition, DB operations
     """
 
-    def __init__(self, par_id=None, comp_id=None, pilot_ID=None, civl_id=None, name=None, sex=None, birthdate=None,
+    def __init__(self, par_id=None, comp_id=None, ID=None, civl_id=None, name=None, sex=None, birthdate=None,
                 nat=None, glider=None, glider_cert=None, sponsor=None, fai_id=None, fai_valid=1, xcontest_id=None,
                 team=None, nat_team=1, paid=None, status=None):
 
         self.par_id                     = par_id            # parPk
         self.comp_id                    = comp_id           # comPk
-        self.ID                         = pilot_ID          # int
+        self.ID                         = ID                # int
         self.civl_id                    = civl_id           # int
         self.name                       = name              # str
         self.sex                        = sex               # 'M', 'F'
@@ -84,3 +84,52 @@ class Participant(object):
             q   = db.session.query(R).get(par_id)
             db.populate_obj(pilot, q)
         return pilot
+
+def import_participants_from_excel(comp_id, filename):
+    '''Gets participants from external file;
+    Returns a list of Participant objects
+    - read Airtribune XSLX file
+
+    excel file format:
+    column name on row 1
+    columns:
+    id,	name,	nat,	female,	birthday,	glider,	color,	sponsor,	fai_licence,	CIVILID,	club,	team,	class
+    '''
+    from logger         import Logger
+    from openpyxl       import load_workbook
+
+    '''create logging and disable output'''
+    Logger('ON', 'import_participants.txt')
+
+    print(f"Comp ID: {comp_id} | filename: {filename}")
+
+    '''load excel file'''
+    try:
+        workbook = load_workbook(filename=filename)
+    except:
+        print('excel file importing error')
+        return
+
+    '''active sheet'''
+    sheet = workbook.active
+
+    '''check validity'''
+    if not (sheet['A1'].value == 'id'):
+        exit()
+
+    pilots = []
+    for row in sheet.iter_rows(min_row=2,
+                               min_col=1,
+                               max_col=13,
+                               values_only=True):
+        if not row[0]:
+            break
+        pil = Participant(ID=row[0], comp_id=comp_id, civl_id=row[9], name=row[1], nat=row[2],
+                sex='F' if row[3]==1 else 'M', glider=row[5], sponsor=row[7],
+                team=row[11], fai_id=row[8], fai_valid=0 if row[8] is None else 1)
+        pilots.append(pil)
+
+    ''' now restore stdout function '''
+    Logger('OFF')
+
+    return pilots
