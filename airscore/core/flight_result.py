f09681811209b6c68668be4fd7fdc71cb79fd146
kuaka <stuart.mackintosh@gmail.com>
2020-02-28 11:19:44 +1300

    updates delete_result method to new db structure

--- airscore/core/flight_result.py
+++ airscore/core/flight_result.py
@@ -849,18 +849,18 @@ def update_status(par_id, task_id, status):
 def delete_result(trackid):
     from trackUtils import get_task_fullpath
     from os import remove
-    from db_tables import tblTaskResult as R
+    from db_tables import TblTaskResult as R
     row_deleted = None
     with Database() as db:
         try:
-            results = db.session.query(R.traFile, R.tasPk).filter(R.tarPk == trackid).one()
-            row_deleted = db.session.query(R.traFile, R.tasPk).filter(R.tarPk == trackid).delete(synchronize_session=False)
+            results = db.session.query(R.track_file, R.task_id).filter(R.track_id == trackid).one()
+            row_deleted = db.session.query(R.track_file, R.task_id).filter(R.track_id == trackid).delete(synchronize_session=False)
         except SQLAlchemyError:
             print("there was a problem deleting the track")
             return None
 
     if results.traFile is not None:
-        full_path = get_task_fullpath(results.tasPk)
-        remove(path.join(full_path, results.traFile))
+        full_path = get_task_fullpath(results.task_id)
+        remove(path.join(full_path, results.track_file))
 
     return row_deleted
