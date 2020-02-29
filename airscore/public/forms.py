d56c00e8fcc0955f5177ae6339dc9cea2a96cd76
Antonio Golfari <antoniogolfari@gmail.com>
2020-03-01 00:27:40 +0100

    Added users view to use with FlaskLogin

--- airscore/public/forms.py
+++ airscore/public/forms.py
@@ -19,22 +19,60 @@ class LoginForm(FlaskForm):
 
     def validate(self):
         """Validate the form."""
+        # from db_tables import User
+        # from myconn import Database
+        import requests
+
         initial_validation = super(LoginForm, self).validate()
         if not initial_validation:
             return False
+
+        # with Database() as db:
+        #     self.user = db.session.query(User).filter_by(username=self.username.data).first()
 
         self.user = User.query.filter_by(username=self.username.data).first()
         if not self.user:
             self.username.errors.append("Unknown username")
             return False
 
-        if not self.user.check_password(self.password.data):
-            self.password.errors.append("Invalid password")
-            return False
-
-        if not self.user.active:
-            self.username.errors.append("User not activated")
+        url = 'https://legapiloti.it/wp-json/wp/v2/users/me'
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+        r = requests.get(url, auth=(self.username.data, self.password.data), headers=headers).json()
+        if not r['id'] == self.user.id:
+            self.username.errors.append("Invalid password")
             return False
         return True
+
+
+# class LoginForm(FlaskForm):
+#     """Login form."""
+#
+#     username = StringField("Username", validators=[DataRequired()])
+#     password = PasswordField("Password", validators=[DataRequired()])
+#
+#     def __init__(self, *args, **kwargs):
+#         """Create instance."""
+#         super(LoginForm, self).__init__(*args, **kwargs)
+#         self.user = None
+#
+#     def validate(self):
+#         """Validate the form."""
+#         initial_validation = super(LoginForm, self).validate()
+#         if not initial_validation:
+#             return False
+#
+#         self.user = User.query.filter_by(username=self.username.data).first()
+#         if not self.user:
+#             self.username.errors.append("Unknown username")
+#             return False
+#
+#         if not self.user.check_password(self.password.data):
+#             self.password.errors.append("Invalid password")
+#             return False
+#
+#         if not self.user.active:
+#             self.username.errors.append("User not activated")
+#             return False
+#         return True
 
 
