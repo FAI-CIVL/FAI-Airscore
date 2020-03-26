0a197fc8e05d2250b3d5ae09173b2d2a25084111
kuaka <stuart.mackintosh@gmail.com>
2020-03-27 00:00:13 +1300

    track upload spinner

--- airscore/static/js/pop_track_admin.js
+++ airscore/static/js/pop_track_admin.js
@@ -28,7 +28,8 @@ function populate_track_admin(task_id){
              +'<button id="DNF' + data.par_id  +'" class="btn btn-primary mt-3" type="button" onclick="set_result(' + data.par_id +',\'dnf\')">Set DNF</button> '
              +'<button id="TrackUp' + data.par_id  +'" class="btnupload btn btn-primary mt-3" onclick="choose_file(' + data.par_id +');">Upload Track</button>'
              +' <div id="filediv' + data.par_id  +'" class = "hideatstart" > <input id="fileupload' + data.par_id +'" type="file" size="chars" class="custom-file-input"  oninput="filesize(this);" data-url="/users/_upload_track/'+ task_id + '/' + data.par_id + '" name="tracklog" >'
-             +'<div id="progress'+ data.par_id+ '" ><div class="bar" style="width: 0%;"><p id="progress_text'+data.par_id +'"></p></div></div></div>';
+             +'<div id="progress'+ data.par_id+ '" ><div class="bar" style="width: 0%;"><p id="spinner'+ data.par_id + '"></p><p id="progress_text'+data.par_id +'"></p></div></div></div>'
+             ;
              return buttons;
             }
             else if (data['Result']=='Processing..') {
