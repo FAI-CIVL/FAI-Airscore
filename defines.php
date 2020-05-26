b1f41d590ff337dd8f436be0cee76e11f5a5c27c
kuaka <stuart.mackintosh@gmail.com>
2020-05-26 12:05:55 +1200

    remove old perl and php code

--- defines.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('BINDIR', '/web/stag/airscore/cgi-bin/');
-define('FILEDIR', '/web/stag/airscore/tracks/');
-define('DATABASE', 'airscore_stag');
-define('MYSQLHOST', 'mysql.legapiloti.dreamhosters.com');
-define('MYSQLUSER', 'airscore_db');
-define('MYSQLPASSWORD', 'Tantobuchi01');
-?>
--- login.php
+++
@@ -1,3 +0,0 @@
-<form action="https://legapiloti.club/oauth/authorize?response_type=code&client_id=di8UK8yOpyXL2GShdbSrTOxwtW3HzanMtwHdEg24&redirect_uri=https://airscore.legapiloti.it/index.php" method="post">
-<input type="submit" value="Log In" \>
-</form>
