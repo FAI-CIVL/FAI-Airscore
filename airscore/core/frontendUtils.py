ccb6da826cb32851bebf675d53c4602dfa4d27c6
Antonio Golfari <antoniogolfari@gmail.com>
2020-07-05 00:16:03 +0200

    case no result file fix

--- airscore/core/frontendUtils.py
+++ airscore/core/frontendUtils.py
@@ -545,17 +545,16 @@ def process_igc_zip(task, zipfile):
 
 def get_task_result_file_list(taskid):
     from db_tables import TblResultFile as R
+    files = []
     with Database() as db:
         try:
-            files = db.session.query(R.created, R.filename, R.status, R.active, R.ref_id).filter(
+            results = db.session.query(R.created, R.filename, R.status, R.active, R.ref_id).filter(
                 R.task_id == taskid).all()
             if files:
-                files = [row._asdict() for row in files]
-
+                files = [row._asdict() for row in results]
         except SQLAlchemyError:
             print("there was a problem with getting the result list")
             return None
-
         return files
 
 
