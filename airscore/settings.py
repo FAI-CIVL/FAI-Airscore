826b82519bd9f7cc0032092f6ea857aa8a65b10f
kuaka <stuart.mackintosh@gmail.com>
2020-10-30 18:18:55 +1300

    remove database uri from .env file etc. avoids required double entry of settings

--- airscore/settings.py
+++ airscore/settings.py
@@ -13,7 +13,7 @@ env.read_env()
 
 ENV = env.str("FLASK_ENV", default="production")
 DEBUG = ENV == "development"
-SQLALCHEMY_DATABASE_URI = env.str("DATABASE_URL")
+SQLALCHEMY_DATABASE_URI = env.str("DATABASE_URI")
 SECRET_KEY = env.str("SECRET_KEY")
 SEND_FILE_MAX_AGE_DEFAULT = env.int("SEND_FILE_MAX_AGE_DEFAULT")
 BCRYPT_LOG_ROUNDS = env.int("BCRYPT_LOG_ROUNDS", default=13)
