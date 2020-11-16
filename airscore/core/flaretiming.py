f00680955cde9cc0f6ccf4221970a1d58a0a4cfe
kuaka <stuart.mackintosh@gmail.com>
2020-11-16 20:53:11 +1300

    km function moved out for DRY. Score updated with more elements completed.

--- airscore/core/flaretiming.py
+++ airscore/core/flaretiming.py
@@ -1,5 +1,11 @@
 from task import get_task_json
 from comp import Comp
+import statistics
+
+
+def km(distance_in_km, decimals=5):
+    """takes an int or float of meters and returns a string in kilometers to {decimals} places with 'km' as a postfix"""
+    return f"{distance_in_km/1000:.{decimals}f} km"
 
 
 def ft_score(comp_id):
@@ -8,11 +14,9 @@ def ft_score(comp_id):
     tasks = comp.get_tasks_details()
     task_results = []
 
-    def km(distance_in_km, decimals=5):
-        return f"{distance_in_km:.{decimals}f} km"
-
     def h(time_in_hours, decimals=5):
-        return f"{time_in_hours:.{decimals}f} h"
+        """takes an int or float of seconds and returns a string in hours to {decimals} places with 'h' as a postfix"""
+        return f"{time_in_hours / (60 * 60):.{decimals}f} h"
 
     def fraction(points, avail_points):
         if not avail_points or (avail_points == 0):
@@ -40,69 +44,17 @@ def ft_score(comp_id):
         stats = task['stats']
         formula = task['formula']
         if task['stats']['fastest_in_goal']:
-            fastest = h(task['stats']['fastest_in_goal'] / (60 * 60))
+            fastest = h(task['stats']['fastest_in_goal'])
 
         bestTime.append(fastest)
-        validity.append({'distance': km(stats['dist_validity']),
-                         'time': stats['time_validity'],
-                         'launch': stats['launch_validity'],
-                         'stop': stats['stop_validity'],
-                         'task': stats['day_quality']})
-        NomDistArea = (((formula['nominal_goal'] + 1) *
-                        (formula['nominal_dist'] / 1000 -
-                         formula['min_dist'] / 1000)) +
-                       max(0, (formula['nominal_goal'] *
-                               stats['max_distance'] / 1000 -
-                               formula['nominal_dist'] / 1000))) / 2
 
-        validityWorkingDistance.append({'area': NomDistArea,
-                                        'flying': stats['pilots_launched'],
-                                        'minimumDistance': km(formula['min_dist'] / 1000),
-                                        'nominalDistance': km(formula['nominal_dist'] / 1000),
-                                        'nominalGoal': formula['nominal_goal'],
-                                        'reachMax': {'extra': km(stats['max_distance'] / 1000),
-                                                     # TODO extra (flown distance + glide bonus in stopped tasks)
-                                                     'flown': km(stats['max_distance'] / 1000)},
-                                        'sum': km(stats['tot_dist_flown'] / 1000)})
-
-        validityWorkingLaunch.append({'flying': stats['pilots_launched'],
-                                      'nominalLaunch': formula['nominal_launch'],
-                                      'present': stats['pilots_present']})
-
-        validityWorkingStop.append({'flying': stats['pilots_launched'],  # TODO
-                                    'landed': 0,  # TODO
-                                    'launchToEssDistance': km(task['info']['SS_distance'], 3),
-                                    'pilotsAtEss': 0,  # TODO
-                                    'reachStats': {
-                                        'extra': {
-                                            'max': '124.018000 km -TODO',  # TODO
-                                            'mean': '37.200457 km -TODO',  # TODO
-                                            'stdDev': '30.230148 km -TODO'  # TODO
-                                        },
-                                        'flown': {
-                                            'max': km(stats['max_distance'] / 1000, 6),  # TODO
-                                            'mean': km(stats['tot_dist_flown'] / stats['pilots_launched'], 6),  # TODO
-                                            'stdDev': '30.230148 km -TODO'  # TODO
-                                        }
-                                    },
-                                    'stillFlying': 94  # TODO
-                                    })
-
-        validityWorkingTime.append({'gsBestTime': 'null-TODO',  # TODO
-                                    'nominalDistance': km(formula['nominal_dist'], 1),
-                                    'nominalTime': h(formula['nominal_time'], 6),
-                                    'reachMax': {
-                                        'extra': km(stats['max_distance'] / 1000),
-                                        # TODO extra (flown distance + glide bonus in stopped tasks)
-                                        'flown': km(stats['max_distance'] / 1000)
-                                    },
-                                    'ssBestTime': 'null-TODO'  # TODO
-                                    })
         results = []
         rank = 1
         prev_score = None
+        distances = []
         for result in task['results']:
             if result['result_type'] != 'abs':
+                distances.append(result['distance_flown'])
                 if not prev_score:
                     rank = 1
                 elif result['score'] < prev_score:
@@ -116,8 +68,8 @@ def ft_score(comp_id):
                                     'leading': result['departure_score'],
                                     'arrival': result['arrival_score'],
                                     'time': result['time_score'],
-                                    'effort': 297.26,
-                                    'reach': 297.18
+                                    'effort': 297.26,  # TODO
+                                    'reach': 297.18  # TODO
                                 },
                                     'place': str(rank),
                                     'total': result['score'],
@@ -126,8 +78,8 @@ def ft_score(comp_id):
                                         'leading': fraction(result['departure_score'], stats['avail_dep_points']),
                                         'arrival': fraction(result['arrival_score'], stats['avail_arr_points']),
                                         'time': fraction(result['time_score'], stats['avail_time_points']),
-                                        'effort': 1,
-                                        'reach': 0.99969724
+                                        'effort': 1,  # TODO
+                                        'reach': 0.99969724  # TODO
                                     },
                                     'reach': {
                                         'extra': km(result['distance'], 6),  # TODO check that this is correct
@@ -137,12 +89,70 @@ def ft_score(comp_id):
                                     'ss': result['SSS_time'],  # TODO FORMAT 2012 - 01 - 05T03: 03:12Z,
                                     'es': result['ESS_time'],
                                     'timeElapsed': result['ss_time'],  # TODO check that this is correct
-                                    'leadingArea': '1261556.0000 km^2 s',
+                                    'leadingArea': '1261556.0000 km ^ 2 s',
                                     'leadingCoef': result['lead_coeff']
                                 }
                                 ])
 
         score.append(results)
+
+        validity.append({'distance': stats['dist_validity'],
+                         'time': stats['time_validity'],
+                         'launch': stats['launch_validity'],
+                         'stop': stats['stop_validity'],
+                         'task': stats['day_quality']})
+
+        NomDistArea = (((formula['nominal_goal'] + 1) *
+                        (formula['nominal_dist'] / 1000 -
+                         formula['min_dist'] / 1000)) +
+                       max(0, (formula['nominal_goal'] *
+                               stats['max_distance'] / 1000 -
+                               formula['nominal_dist'] / 1000))) / 2
+
+        validityWorkingDistance.append({'area': NomDistArea,
+                                        'flying': stats['pilots_launched'],
+                                        'minimumDistance': km(formula['min_dist']),
+                                        'nominalDistance': km(formula['nominal_dist']),
+                                        'nominalGoal': formula['nominal_goal'],
+                                        'reachMax': {'extra': km(stats['max_distance']),
+                                                     # TODO extra (flown distance + glide bonus in stopped tasks)
+                                                     'flown': km(stats['max_distance'])},
+                                        'sum': km(stats['tot_dist_flown'])})
+
+        validityWorkingLaunch.append({'flying': stats['pilots_launched'],
+                                      'nominalLaunch': formula['nominal_launch'],
+                                      'present': stats['pilots_present'],
+                                      })
+
+        validityWorkingStop.append({'flying': stats['pilots_launched'],
+                                    'landed': stats['pilots_landed'],
+                                    'launchToEssDistance': km(task['info']['SS_distance'], 3),
+                                    'pilotsAtEss': stats['pilots_ess'],
+                                    'reachStats': {
+                                        'extra': {
+                                            'max': km(stats['max_distance'], 6),
+                                            'mean': km(statistics.mean(distances), 6),
+                                            'stdDev': km(statistics.stdev(distances), 6),
+                                        },
+                                        'flown': {
+                                            'max': km(stats['max_distance'], 6),
+                                            'mean': km(stats['tot_dist_flown'] / stats['pilots_launched'], 6),
+                                            'stdDev': km(statistics.stdev(distances), 6),
+                                        }
+                                    },
+                                    'stillFlying': stats['pilots_launched'] - stats['pilots_landed']
+                                    })
+
+        validityWorkingTime.append({'gsBestTime': 'null-TODO',  # TODO
+                                    'nominalDistance': km(formula['nominal_dist'], 1),
+                                    'nominalTime': h(formula['nominal_time'], 6),
+                                    'reachMax': {
+                                        'extra': km(stats['max_distance']),
+                                        # TODO extra (flown distance + glide bonus in stopped tasks)
+                                        'flown': km(stats['max_distance']),
+                                    },
+                                    'ssBestTime': 'null-TODO'  # TODO
+                                    })
 
     return {'bestTime': bestTime,
             'validity': validity,
@@ -156,9 +166,6 @@ def ft_score(comp_id):
 def ft_route(comp_id):
     from numpy import cumsum
     from task import Task
-
-    def km(distance_in_km, decimals=5):
-        return f"{distance_in_km:.{decimals}f} km"
 
     comp = Comp(comp_id)
     tasks = comp.get_tasks_details()
@@ -178,17 +185,17 @@ def ft_route(comp_id):
             opt_wpts.append({'lat': tp.lat,
                              'lon': tp.lon})
 
-        point = {'distance': km(task.distance/1000),
-                 'legs': [km(l / 1000) for l in task.legs],
-                 'legsSum': [km(l / 1000) for l in cumsum(task.legs).tolist()],
-                 'flipSum': [km(l / 1000) for l in cumsum(task.legs[::-1]).tolist()[::-1]],
+        point = {'distance': km(task.distance),
+                 'legs': [km(leg) for leg in task.legs],
+                 'legsSum': [km(leg) for leg in cumsum(task.legs).tolist()],
+                 'flipSum': [km(leg) for leg in cumsum(task.legs[::-1]).tolist()[::-1]],
                  'waypoints': wpts}
 
-        ellipse = {'distance': km(task.opt_dist/1000),
-                 'legs': [km(l / 1000) for l in task.optimised_legs],
-                 'legsSum': [km(l / 1000) for l in cumsum(task.optimised_legs).tolist()],
-                 'flipSum': [km(l / 1000) for l in cumsum(task.optimised_legs[::-1]).tolist()[::-1]],
-                 'waypoints': opt_wpts}
+        ellipse = {'distance': km(task.opt_dist),
+                   'legs': [km(leg) for leg in task.optimised_legs],
+                   'legsSum': [km(leg) for leg in cumsum(task.optimised_legs).tolist()],
+                   'flipSum': [km(leg) for leg in cumsum(task.optimised_legs[::-1]).tolist()[::-1]],
+                   'waypoints': opt_wpts}
         route.append({'point': point,
                       'ellipse': ellipse})
 
