b4d170d18dfd65bc905e68bd9c9ad9a5da11fcc7
kuaka <stuart.mackintosh@gmail.com>
2020-12-15 21:29:43 +1300

    fix form for changed enum

--- airscore/user/forms.py
+++ airscore/user/forms.py
@@ -230,7 +230,7 @@ class TaskForm(FlaskForm):
     comment = StringField('Comment', description='Sometimes you may wish to make a comment that will show up'
                                                  ' in the competition overview page. e.g. "task stopped at 14:34"')
     date = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()], default=date.today)
-    task_type = SelectField('Type', choices=[('race', 'Race'), ('elapsed_time', 'Elapsed time')])
+    task_type = SelectField('Type', choices=[('race', 'Race'), ('elapsed time', 'Elapsed time')])
     region = SelectField('Region', id='select_region', choices=[(0, ' -')], default=0, coerce=int,
                          validators=[Optional()], description='Determines the Waypoint listed, and the airspace used')
 
