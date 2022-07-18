26918e61641410290fc3edae570c2228d3a52840
Olivier Diserens <olivier@diserens.ch>
2022-07-18 13:54:33 +0200

    Added SSH daemon for production Docker image

--- shell_scripts/supervisord_entrypoint.sh
+++ shell_scripts/supervisord_entrypoint.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env sh
 set -e
 
-npm run build
+#npm run build
+#
+#if [ $# -eq 0 ] || [ "${1#-}" != "$1" ]; then
+#  set -- supervisord "$@"
+#fi
+#
+###############
+## SSH setup ##
+##
 
-if [ $# -eq 0 ] || [ "${1#-}" != "$1" ]; then
-  set -- supervisord "$@"
+mkdir -p /app/data/ssh/etc /app/data/ssh/run
+cp /etc/ssh/sshd_config /app/data/ssh/etc/
+echo "Port 1220" >> /app/data/ssh/etc/sshd_config
+echo "HostKey /app/data/ssh/etc/ssh_host_rsa_key" >> /app/data/ssh/etc/sshd_config
+echo "PidFile /app/data/ssh/sshd.pid" >> /app/data/ssh/etc/sshd_config
+echo "AuthorizedKeysFile /app/data/ssh/authorized_keys" >> /app/data/ssh/etc/sshd_config
+echo "PrintMotd no" >> /app/data/ssh/etc/sshd_config
+echo "PrintLastLog no" >> /app/data/ssh/etc/sshd_config
+
+if [ ! -f /app/data/ssh/etc/ssh_host_rsa_key ]; then
+    ssh-keygen -t rsa -f /app/data/ssh/etc/ssh_host_rsa_key  -N ''
 fi
+
+### and start sshd
+/usr/sbin/sshd -p1220 -f /app/data/ssh/etc/sshd_config -D -e &
 
 exec "$@"
