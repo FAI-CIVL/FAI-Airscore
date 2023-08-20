936c253ca28de406dc8c81df5493350114880c52
Antonio Golfari <antoniogolfari@gmail.com>
2023-08-20 21:59:50 +0200

    fixed matrix out of index error

--- airscore/core/formulas/lclib/weightedarea.py
+++ airscore/core/formulas/lclib/weightedarea.py
@@ -152,7 +152,7 @@ def tot_lc_calculation_integrate(res, t) -> float:
             # matrix = weight_matrix()
             ratio = c_round(toDo(best_dist_to_ess, ss_distance), integrate_precision)
             # slice_dist = ss_distance / len(matrix)
-            index = int(ratio * len(t.formula.matrix))
+            index = min(len(t.formula.matrix) - 1, int(ratio * len(t.formula.matrix)))  # avoid out of range if pilot bombed out at Start
             missing_time = t.max_time - t.start_time
             landed_out = sum(weight * t.formula.slice_dist * missing_time for weight in missing_area_integrate(t.formula.matrix, index, 0))
     return (res.fixed_LC + landed_out) / (1800 * ss_distance)
